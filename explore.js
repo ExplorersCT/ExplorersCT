803f0ce2388f53f85a8cee6a7329b004bea0771a
ExplorersCT <explorersct@live.com>
2019-02-24 15:27:42 +0200

    changes to the share button, again

--- explore.js
+++ explore.js
@@ -1,1 +1,21 @@
-// alert("Hi explorer!");
+var exploreButton = document.getElementById('explore-button');
+var iframe = document.getElementById('iframe');
+var randomPages = [
+    "https://www.youtube.com/embed/g9VjiTDbVDk",
+    "https://www.youtube.com/embed/-0gED3rn2Tc",
+    "https://www.youtube.com/embed/T-RtG5Z-9jQ"];
+var randomPage = randomPages[Math.floor(Math.random()*randomPages.length)];
+
+// Display a random iframe source at the first visit
+iframe.src = randomPage;
+
+//when pressing the Explore button
+exploreButton.addEventListener('click', function() {
+
+    //get random page 
+    randomPage = randomPages[Math.floor(Math.random()*randomPages.length)];
+
+    //change the iframe source
+    iframe.src = randomPage;
+
+});
